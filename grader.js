03a72515b97a761cc1a1a7ddaad16bcb5ed6f60f
CWM <wmchoe@gmail.com>
2013-07-13 15:41:48 +0000

    modified grader.js

--- grader.js
+++ grader.js
@@ -26,6 +26,7 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://morning-springs-5756.herokuapp.com/";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -34,6 +35,15 @@ var assertFileExists = function(infile) {
         process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
     return instr;
+};
+
+var result ="";
+
+var assertURL = function(url) {
+    var rest = require('restler');
+    rest.get(url).on('complete',function(result){
+        console.log(result);
+        return result;
 };
 
 var cheerioHtmlFile = function(htmlfile) {
@@ -65,6 +75,7 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>','URL address', clone(assertFileExists),URL_DEFAULT)
         .parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
